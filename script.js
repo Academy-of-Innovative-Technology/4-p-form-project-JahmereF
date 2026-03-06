39a903042ab9a2a2dfbd900bbd23970b7f460a40
Jahmere Fernandez <jahmeref2@nycstudents.net>
2026-03-06 12:34:39 -0500

    Complete

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('registrationForm');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        alert('Form submission was successful! Welcome to GameCon 2026!');
+        
+        console.log('Form Submitted');
+    });
+});
 
